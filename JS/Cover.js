671a966714aefb6d7d4236a9d3bb05cb835c72ee
AmayaRoumimper <amayaroumimper@gmail.com>
2026-08-23 10:14:42 +0200

    Add files via upload

---
+++ JS/Cover.js
@@ -0,0 +1,24 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+            document.getElementById("login").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        })
+        .catch(() => {
+            document.getElementById("loginError").style.display = "block";
+        });
+});
+
+// Automatisch ingelogd blijven
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    } else {
+        document.getElementById("login").style.display = "block";
+        document.getElementById("content").style.display = "none";
+    }
+});
